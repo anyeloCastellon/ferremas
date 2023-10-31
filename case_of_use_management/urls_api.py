9c361d317b8e233710c0f281ac4ec076f3c4d323
Anyelo Castellon <anyelo.kine.castellon@gmail.com>
2023-10-31 16:48:03 -0300

    vistas personalizadas para la vista especifica de cliente por ID id_company

--- case_of_use_management/urls_api.py
+++ case_of_use_management/urls_api.py
@@ -1,6 +1,16 @@
 from django.urls import path
-from .views_api import CaseOfUseDataView
+from .views_api import CaseOfUseDataView, CaseOfUseDataDynamicCompanyView, CaseOfUseTechnologyCoverageView, CaseOfUseMissingTechnologyView, CaseOfUseRuleTypeCoverageView, CaseOfUseMissingRuleTypeView, CaseOfUseIncidentCoverageView, CaseOfUseMissingIncidentView, CaseOfUseIncidentTypeCoverageView, CaseOfUseMissingIncidentTypeView
 
 urlpatterns = [
     path('case_of_use_data/', CaseOfUseDataView.as_view(), name='case_of_use_data'),
+    path('case_of_use_data/unassigned_caseofuse/<int:company_id>/', CaseOfUseDataDynamicCompanyView.as_view(), name='unassigned_caseofuse'),
+    path('case_of_use_data/technology_coverage/<int:company_id>/', CaseOfUseTechnologyCoverageView.as_view(), name='technology_coverage'),
+    path('case_of_use_data/missing_technology_coverage/<int:company_id>/', CaseOfUseMissingTechnologyView.as_view(), name='missing_technology_coverage'),
+    path('case_of_use_data/rule_type_coverage/<int:company_id>/', CaseOfUseRuleTypeCoverageView.as_view(), name='rule_type_coverage'),
+    path('case_of_use_data/missing_rule_type_coverage/<int:company_id>/', CaseOfUseMissingRuleTypeView.as_view(), name='missing_rule_type_coverage'),
+    path('case_of_use_data/missing_incident_coverage/<int:company_id>/', CaseOfUseIncidentCoverageView.as_view(), name='missing_incident_coverage'),
+    path('case_of_use_data/incident_coverage/<int:company_id>/', CaseOfUseMissingIncidentView.as_view(), name='incident_coverage'),
+    path('case_of_use_data/incident_type_coverage/<int:company_id>/', CaseOfUseMissingIncidentTypeView.as_view(), name='case_of_use_incident_type_coverage'),
+    path('case_of_use_data/missing_incident_type_coverage/<int:company_id>/', CaseOfUseIncidentTypeCoverageView.as_view(), name='case_of_use_missing_incident_type_coverage'),
+
 ]
--- case_of_use_management/views_api.py
+++ case_of_use_management/views_api.py
@@ -42,3 +42,212 @@ class CaseOfUseDataView(APIView):
             columns = [col[0] for col in cursor.description]
             data = [dict(zip(columns, row)) for row in cursor.fetchall()]
         return Response(data)
+
+class CaseOfUseDataDynamicCompanyView(APIView):
+
+    def get(self, request, company_id):
+        with connection.cursor() as cursor:
+            cursor.execute(f"""
+                SELECT coumcu.id_case_of_use as "ID Caso de Uso", 
+                       coumcu.name_case_of_use as "Nombre Caso de Uso", 
+                       coumcu.is_created as "Creado"
+                FROM case_of_use_management_caseuse as coumcu
+                LEFT JOIN (
+                    SELECT caseuse_id
+                    FROM case_of_use_management_caseuse_company
+                    WHERE company_id = {company_id}
+                ) AS assigned_cases ON coumcu.id_case_of_use = assigned_cases.caseuse_id
+                WHERE assigned_cases.caseuse_id IS NULL;
+            """)
+            columns = [col[0] for col in cursor.description]
+            data = [dict(zip(columns, row)) for row in cursor.fetchall()]
+        return Response(data)
+
+
+class CaseOfUseTechnologyCoverageView(APIView):
+
+    def get(self, request, company_id):
+        with connection.cursor() as cursor:
+            cursor.execute(f"""
+                SELECT 
+                    coumtt.id_type_tech as "ID Tipo de Tecnologia", 
+                    coumtt.name_type_tech as "Nombre Tipo de Tecnologia"
+                FROM case_of_use_management_typetech as coumtt
+                INNER JOIN case_of_use_management_caseuse_type_tech as coumcutt ON coumcutt.typetech_id = coumtt.id_type_tech
+                INNER JOIN case_of_use_management_caseuse as cumc ON cumc.id_case_of_use = coumcutt.caseuse_id
+                INNER JOIN case_of_use_management_caseuse_company as coumcuc ON cumc.id_case_of_use = coumcuc.caseuse_id
+                INNER JOIN user_management_company as umc ON umc.id_company = coumcuc.company_id
+                WHERE umc.id_company = {company_id}
+                GROUP BY coumtt.id_type_tech
+                ORDER BY coumtt.id_type_tech
+            """)
+            columns = [col[0] for col in cursor.description]
+            data = [dict(zip(columns, row)) for row in cursor.fetchall()]
+        return Response(data)
+
+
+
+class CaseOfUseMissingTechnologyView(APIView):
+
+    def get(self, request, company_id):
+        with connection.cursor() as cursor:
+            cursor.execute(f"""
+                SELECT 
+                    coumtt.id_type_tech as "ID Tipo de Tecnologia", 
+                    coumtt.name_type_tech as "Nombre Tipo de Tecnologia"
+                FROM case_of_use_management_typetech as coumtt
+                WHERE coumtt.id_type_tech NOT IN (
+                    SELECT DISTINCT coumcutt.typetech_id
+                    FROM case_of_use_management_caseuse_type_tech as coumcutt
+                    INNER JOIN case_of_use_management_caseuse as coumcu ON coumcutt.caseuse_id = coumcu.id_case_of_use
+                    INNER JOIN case_of_use_management_caseuse_company as coucuc ON coumcu.id_case_of_use = coucuc.caseuse_id
+                    WHERE coucuc.company_id = {company_id}
+                    );
+            """)
+            columns = [col[0] for col in cursor.description]
+            data = [dict(zip(columns, row)) for row in cursor.fetchall()]
+        return Response(data)
+
+
+
+class CaseOfUseRuleTypeCoverageView(APIView):
+
+    def get(self, request, company_id):
+        with connection.cursor() as cursor:
+            cursor.execute(f"""
+                SELECT 
+                    coumtr.id_type_rule as "ID Tipo de Regla", 
+                    coumtr.name_type_rule as "Nombre Tipo de Regla"
+                FROM case_of_use_management_typerule as coumtr
+                INNER JOIN case_of_use_management_caseuse_type_rule as coumcutr ON coumcutr.typerule_id = coumtr.id_type_rule 
+                INNER JOIN case_of_use_management_caseuse as cumc ON cumc.id_case_of_use = coumcutr.caseuse_id
+                INNER JOIN case_of_use_management_caseuse_company as coumcuc ON cumc.id_case_of_use = coumcuc.caseuse_id
+                INNER JOIN user_management_company as umc ON umc.id_company = coumcuc.company_id
+                WHERE umc.id_company = {company_id}
+                GROUP BY coumtr.id_type_rule
+                ORDER BY coumtr.id_type_rule
+            """)
+            columns = [col[0] for col in cursor.description]
+            data = [dict(zip(columns, row)) for row in cursor.fetchall()]
+        return Response(data)
+
+
+
+class CaseOfUseMissingRuleTypeView(APIView):
+
+    def get(self, request, company_id):
+        with connection.cursor() as cursor:
+            cursor.execute(f"""
+                    SELECT 
+                        coumtr.id_type_rule as "ID Tipo de Regla", 
+                        coumtr.name_type_rule as "Nombre Tipo de Regla"
+                    FROM case_of_use_management_typerule as coumtr
+                    LEFT JOIN (
+                        SELECT DISTINCT coumcutr.typerule_id
+                        FROM case_of_use_management_caseuse_type_rule as coumcutr
+                        INNER JOIN case_of_use_management_caseuse as cumc ON coumcutr.caseuse_id = cumc.id_case_of_use
+                        INNER JOIN case_of_use_management_caseuse_company as coumcuc ON cumc.id_case_of_use = coumcuc.caseuse_id
+                        WHERE coumcuc.company_id = {company_id}
+                    ) AS assigned_rules ON coumtr.id_type_rule = assigned_rules.typerule_id
+                    WHERE assigned_rules.typerule_id IS NULL;
+            """)
+            columns = [col[0] for col in cursor.description]
+            data = [dict(zip(columns, row)) for row in cursor.fetchall()]
+        return Response(data)
+
+
+
+class CaseOfUseIncidentCoverageView(APIView):
+
+    def get(self, request, company_id):
+        with connection.cursor() as cursor:
+            cursor.execute(f"""
+                SELECT 
+                    ihmi.id_incident as "ID Incidente", 
+                    ihmi.name_incident as "Nombre Incidente"
+                FROM incident_handler_management_incident as ihmi
+                LEFT JOIN (
+                    SELECT DISTINCT cucci.incident_id
+                    FROM case_of_use_management_caseuse_incident as cucci
+                    INNER JOIN case_of_use_management_caseuse as cu ON cucci.caseuse_id = cu.id_case_of_use
+                    INNER JOIN case_of_use_management_caseuse_company as cucc ON cu.id_case_of_use = cucc.caseuse_id
+                    WHERE cucc.company_id = {company_id}
+                ) AS assigned_incidents ON ihmi.id_incident = assigned_incidents.incident_id
+                WHERE assigned_incidents.incident_id IS NULL;
+            """)
+            columns = [col[0] for col in cursor.description]
+            data = [dict(zip(columns, row)) for row in cursor.fetchall()]
+        return Response(data)
+
+
+
+
+class CaseOfUseMissingIncidentView(APIView):
+
+    def get(self, request, company_id):
+        with connection.cursor() as cursor:
+            cursor.execute(f"""
+                    SELECT 
+                        ihmi.id_incident as "ID Incidente", 
+                        ihmi.name_incident as "Nombre Incidente"
+                    FROM incident_handler_management_incident as ihmi
+                    INNER JOIN (
+                        SELECT DISTINCT cucci.incident_id
+                        FROM case_of_use_management_caseuse_incident as cucci
+                        INNER JOIN case_of_use_management_caseuse as cu ON cucci.caseuse_id = cu.id_case_of_use
+                        INNER JOIN case_of_use_management_caseuse_company as cucc ON cu.id_case_of_use = cucc.caseuse_id
+                        WHERE cucc.company_id = {company_id}
+                    ) AS assigned_incidents ON ihmi.id_incident = assigned_incidents.incident_id;
+            """)
+            columns = [col[0] for col in cursor.description]
+            data = [dict(zip(columns, row)) for row in cursor.fetchall()]
+        return Response(data)
+
+
+
+
+
+class CaseOfUseIncidentTypeCoverageView(APIView):
+
+    def get(self, request, company_id):
+        with connection.cursor() as cursor:
+            cursor.execute(f"""
+                SELECT 
+                    ihmti.id_type_incident as "ID Tipo de Incidente", 
+                    ihmti.name_type_incident as "Nombre Tipo de Incidente"
+                FROM incident_handler_management_typeincident as ihmti
+                LEFT JOIN (
+                    SELECT DISTINCT cucti.typeincident_id
+                    FROM case_of_use_management_caseuse_type_incident as cucti
+                    INNER JOIN case_of_use_management_caseuse as cu ON cucti.caseuse_id = cu.id_case_of_use
+                    INNER JOIN case_of_use_management_caseuse_company as cucc ON cu.id_case_of_use = cucc.caseuse_id
+                    WHERE cucc.company_id = {company_id}
+                ) AS assigned_incidents ON ihmti.id_type_incident = assigned_incidents.typeincident_id
+                WHERE assigned_incidents.typeincident_id IS NULL;
+            """)
+            columns = [col[0] for col in cursor.description]
+            data = [dict(zip(columns, row)) for row in cursor.fetchall()]
+        return Response(data)
+
+
+class CaseOfUseMissingIncidentTypeView(APIView):
+
+    def get(self, request, company_id):
+        with connection.cursor() as cursor:
+            cursor.execute(f"""
+                    SELECT 
+                        ihmti.id_type_incident as "ID Tipo de Incidente", 
+                        ihmti.name_type_incident as "Nombre Tipo de Incidente"
+                    FROM incident_handler_management_typeincident as ihmti
+                    INNER JOIN (
+                        SELECT DISTINCT cucti.typeincident_id
+                        FROM case_of_use_management_caseuse_type_incident as cucti
+                        INNER JOIN case_of_use_management_caseuse as cu ON cucti.caseuse_id = cu.id_case_of_use
+                        INNER JOIN case_of_use_management_caseuse_company as cucc ON cu.id_case_of_use = cucc.caseuse_id
+                        WHERE cucc.company_id = {company_id}
+                    ) AS assigned_incidents ON ihmti.id_type_incident = assigned_incidents.typeincident_id;
+            """)
+            columns = [col[0] for col in cursor.description]
+            data = [dict(zip(columns, row)) for row in cursor.fetchall()]
+        return Response(data)
+
